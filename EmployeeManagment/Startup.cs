6b47d171a5e8fb39fcd8e5b0bca0a1fcc803d300
Asato <asato_chen@outlook.com>
2020-03-07 17:14:06 +0800

    add  middleware and mvc practices

--- EmployeeManagment/Startup.cs
+++ EmployeeManagment/Startup.cs
@@ -5,15 +5,18 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace EmployeeManagment
 {
     public class Startup
     {
         private IConfiguration _config;
+        
 
         public Startup(IConfiguration config)
         {
@@ -22,7 +25,8 @@ namespace EmployeeManagment
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
-        {
+        {            
+            services.AddMvc(options => options.EnableEndpointRouting =false);            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,8 +34,19 @@ namespace EmployeeManagment
         {
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                DeveloperExceptionPageOptions developerExceptionPageOptions = new DeveloperExceptionPageOptions();
+                developerExceptionPageOptions.SourceCodeLineCount = 1;
+                app.UseDeveloperExceptionPage(developerExceptionPageOptions);
             }
+
+
+            //FileServerOptions fileserveroptions = new FileServerOptions();
+            //fileserveroptions.DefaultFilesOptions.DefaultFileNames.Clear();
+            //fileserveroptions.DefaultFilesOptions.DefaultFileNames.Add("foo.html");
+            //app.UseFileServer(fileserveroptions);
+
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
 
             app.UseRouting();
 
@@ -39,11 +54,12 @@ namespace EmployeeManagment
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    //await context.Response.WriteAsync("Hello World!");
+                    
+                    await context.Response.WriteAsync("Hello World!");
                     //await context.Response.WriteAsync(System.Diagnostics.Process.GetCurrentProcess().ProcessName);
-                    await context.Response.WriteAsync(_config["MyKey"]);
+                    //await context.Response.WriteAsync(_config["MyKey"]);
                 });
-            });
+            });           
         }
     }
 }
